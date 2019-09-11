fc4a19a8730c7d26ad42f7ea53f4a42ed64e19bb
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-09-11 10:19:51 +0200

    add pokemon task

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Pokemon from './Pokemon';
+import PokeList from './components/PokeList';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      Pokemon:Pokemon
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PokeList Pokemon={this.state.Pokemon}/>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class Pokemon extends React.Component {
+    render() {
+        return(
+    <div>
+        <div className="card__list-container">
+        <img src={this.props.imagen} alt={this.props.name} className="card__list-container-img"/>
+        <p className="card__list-container-name">{this.props.name}</p>
+    <ul className="card__list-container-list_properties">{this.props.types.map((type,index) => {
+    return (
+        <li className="card__list-container-list_properties-item" key={index}>
+        <div className="card__list-list_properties-container_text">
+        <p className="card__list-container-text">{type}</p>
+        </div>
+        </li>);
+    })
+    }
+        </ul>
+        </div>
+        </div>
+        )
+    }
+}
+
+export default Pokemon;
